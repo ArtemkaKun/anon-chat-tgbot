731451590bce7bc5eac2e3ab37d64b0683b621c1
ArtemkaKun <zumikq78@gmail.com>
2019-12-21 09:00:38 +0100

    Description added

--- main.go
+++ main.go
@@ -69,7 +69,7 @@ func BotUpdateLoop(my_bot *tgbotapi.BotAPI, database *sql.DB) {
 						msg.Text = "You are chatting already"
 					}
 				} else {
-					msg.Text = "You need to register first"
+					msg.Text = "You need /start first"
 				}
 
 				_, err := my_bot.Send(msg)
@@ -98,7 +98,7 @@ func BotUpdateLoop(my_bot *tgbotapi.BotAPI, database *sql.DB) {
 						msg.Text = "You are not chatting now!"
 					}
 				} else {
-					msg.Text = "You need to register first"
+					msg.Text = "You need to /start first"
 				}
 
 				_, err := my_bot.Send(msg)
@@ -176,7 +176,7 @@ func BotUpdateLoop(my_bot *tgbotapi.BotAPI, database *sql.DB) {
 						ErrorCatch(err.Error(), my_bot)
 					}
 				} else {
-					msg.Text = "Bot cannot send this shit yet! Please, contact with creator"
+					msg.Text = "Bot cannot send this yet! Please, contact with creator"
 					_, err := my_bot.Send(msg)
 					if err != nil {
 						ErrorCatch(err.Error(), my_bot)
@@ -195,10 +195,22 @@ func BotUpdateLoop(my_bot *tgbotapi.BotAPI, database *sql.DB) {
 		case "start":
 			if !CheckReg(update.Message.From.ID, database, my_bot) {
 				FirstStart(update.Message.From.ID, database, my_bot)
-				msg.Text = "You were registered"
+				msg.Text = "Hello, this is Freedom chat, where you can freely express your minds and talk with other strangers\n" +
+					"To start the chat, send /go_chat command or press \"New chat\" button\n" +
+					"To leave the chat, send /leave_chat command or press \"Leave chat\" button\n" +
+					"Bot doesn't store any personal data, so chats are fully anonymous" +
+					"If You want to check how the bot works - check my video (https://www.youtube.com/watch?v=drtAdOByW54&t=1s)" +
+					"If You have some questions or suggestions, please, feel free to contact with me, @YUART\n" +
+					"Also, check my Patreon page (https://www.patreon.com/artemkakun) if you want receive some bonuses from me :)\n"
 				msg.ReplyMarkup = numericKeyboard
 			} else {
-				msg.Text = "You have registered already"
+				msg.Text = "Hello, this is Freedom chat, where you can freely express your minds and talk with other strangers\n" +
+					"To start the chat, send /go_chat command or press \"New chat\" button\n" +
+					"To leave the chat, send /leave_chat command or press \"Leave chat\" button\n" +
+					"Bot doesn't store any personal data, so chats are fully anonymous" +
+					"If You want to check how the bot works - check my video (https://www.youtube.com/watch?v=drtAdOByW54&t=1s)" +
+					"If You have some questions or suggestions, please, feel free to contact with me, @YUART\n" +
+					"Also, check my Patreon page (https://www.patreon.com/artemkakun) if you want receive some bonuses from me :)\n"
 				msg.ReplyMarkup = numericKeyboard
 			}
 		case "go_chat":
@@ -214,7 +226,7 @@ func BotUpdateLoop(my_bot *tgbotapi.BotAPI, database *sql.DB) {
 					msg.Text = "You are chatting already"
 				}
 			} else {
-				msg.Text = "You need to register first"
+				msg.Text = "You need to /start first"
 			}
 		case "leave_chat":
 			if CheckReg(update.Message.From.ID, database, my_bot) {
@@ -234,7 +246,7 @@ func BotUpdateLoop(my_bot *tgbotapi.BotAPI, database *sql.DB) {
 					msg.Text = "You are not chatting now!"
 				}
 			} else {
-				msg.Text = "You need to register first"
+				msg.Text = "You need to /start first"
 			}
 		}
 
