5fc6f9352dedcdd03401d8b8386fff8ccfa8e901
ArtemkaKun <zumikq78@gmail.com>
2020-04-15 22:33:32 +0200

    Add: Bot.go

---
+++ Bot.go
@@ -0,0 +1,251 @@
+package main
+
+import (
+	"database/sql"
+	"fmt"
+	"github.com/Syfaro/telegram-bot-api"
+	"log"
+)
+
+var Bot *tgbotapi.BotAPI
+
+var chatControlKeyboard = tgbotapi.NewReplyKeyboard(
+	tgbotapi.NewKeyboardButtonRow(
+		tgbotapi.NewKeyboardButton("New chat"),
+		tgbotapi.NewKeyboardButton("Leave chat"),
+	),
+)
+
+func init() {
+	bot, err := tgbotapi.NewBotAPI("NEW BOT TOKEN HERE")
+	if err != nil {
+		BotInitError(err)
+	}
+
+	log.Printf("Autorised on account %s", bot.Self.UserName)
+}
+
+func BotUpdateLoop(my_bot *tgbotapi.BotAPI, database *sql.DB) {
+	u := tgbotapi.NewUpdate(0)
+	u.Timeout = 60
+
+	updates, err := my_bot.GetUpdatesChan(u)
+	if err != nil {
+		log.Panic(err)
+	}
+
+	for update := range updates {
+
+		if update.Message == nil {
+			continue
+		}
+
+		if !update.Message.IsCommand() {
+			switch update.Message.Text {
+			case "New chat":
+				msg := tgbotapi.NewMessage(int64(update.Message.From.ID), "")
+
+				if CheckUserReg(update.Message.From.ID, database, my_bot) {
+					if IsFree(update.Message.From.ID, database, my_bot) {
+						if !IsSearch(update.Message.From.ID, database, my_bot) {
+							ChangeSearch(database, update.Message.From.ID, 1, my_bot)
+							msg.Text = "Search started"
+						} else {
+							msg.Text = "You are searching already"
+						}
+					} else {
+						msg.Text = "You are chatting already"
+					}
+				} else {
+					msg.Text = "You need /start first"
+				}
+
+				_, err := my_bot.Send(msg)
+				if err != nil {
+					ErrorCatch(err.Error(), my_bot)
+				}
+				continue
+
+			case "Leave chat":
+				msg := tgbotapi.NewMessage(int64(update.Message.From.ID), "")
+
+				if CheckUserReg(update.Message.From.ID, database, my_bot) {
+					if FindChat(update.Message.From.ID, database, my_bot) != 0 {
+						chat_id := FindChat(update.Message.From.ID, database, my_bot)
+						DeleteChat(update.Message.From.ID, database, my_bot)
+						ChangeState(database, update.Message.From.ID, 0, my_bot)
+						msg.Text = "You leaved a chat"
+
+						DeleteChat(chat_id, database, my_bot)
+						ChangeState(database, chat_id, 0, my_bot)
+						_, err := my_bot.Send(tgbotapi.NewMessage(int64(chat_id), "The stranger leave the chat"))
+						if err != nil {
+							ErrorCatch(err.Error(), my_bot)
+						}
+					} else {
+						msg.Text = "You are not chatting now!"
+					}
+				} else {
+					msg.Text = "You need to /start first"
+				}
+
+				_, err := my_bot.Send(msg)
+				if err != nil {
+					ErrorCatch(err.Error(), my_bot)
+				}
+				continue
+			}
+
+			if FindChat(update.Message.From.ID, database, my_bot) != 0 {
+				chat_id := FindChat(update.Message.From.ID, database, my_bot)
+				msg := tgbotapi.NewMessage(int64(chat_id), "")
+
+				msg.Text = update.Message.Text
+				if msg.Text != "" {
+					_, err := my_bot.Send(msg)
+					if err != nil {
+						ErrorCatch(err.Error(), my_bot)
+					}
+				} else if update.Message.Photo != nil {
+					photo := tgbotapi.NewPhotoShare(int64(chat_id), "")
+					photo.FileID = (*update.Message.Photo)[2].FileID
+					_, err := my_bot.Send(photo)
+					if err != nil {
+						ErrorCatch(err.Error(), my_bot)
+					}
+				} else if update.Message.Voice != nil {
+					voice := tgbotapi.NewVoiceShare(int64(chat_id), "")
+					voice.FileID = update.Message.Voice.FileID
+					_, err := my_bot.Send(voice)
+					if err != nil {
+						ErrorCatch(err.Error(), my_bot)
+					}
+				} else if update.Message.Animation != nil {
+					voice := tgbotapi.NewAnimationShare(int64(chat_id), "")
+					voice.FileID = update.Message.Animation.FileID
+					_, err := my_bot.Send(voice)
+					if err != nil {
+						ErrorCatch(err.Error(), my_bot)
+					}
+				} else if update.Message.Audio != nil {
+					audio := tgbotapi.NewAudioShare(int64(chat_id), "")
+					audio.FileID = update.Message.Audio.FileID
+					_, err := my_bot.Send(audio)
+					if err != nil {
+						ErrorCatch(err.Error(), my_bot)
+					}
+				} else if update.Message.Sticker != nil {
+					sticker := tgbotapi.NewStickerShare(int64(chat_id), "")
+					sticker.FileID = update.Message.Sticker.FileID
+					_, err := my_bot.Send(sticker)
+					if err != nil {
+						ErrorCatch(err.Error(), my_bot)
+					}
+				} else if update.Message.Document != nil {
+					doc := tgbotapi.NewDocumentShare(int64(chat_id), "")
+					doc.FileID = update.Message.Document.FileID
+					_, err := my_bot.Send(doc)
+					if err != nil {
+						ErrorCatch(err.Error(), my_bot)
+					}
+				} else if update.Message.Video != nil {
+					video := tgbotapi.NewVideoShare(int64(chat_id), "")
+					video.FileID = update.Message.Video.FileID
+					_, err := my_bot.Send(video)
+					if err != nil {
+						ErrorCatch(err.Error(), my_bot)
+					}
+				} else if update.Message.VideoNote != nil {
+					video_note := tgbotapi.NewVideoNoteShare(int64(chat_id), 0, "")
+					video_note.FileID = update.Message.VideoNote.FileID
+					video_note.Length = update.Message.VideoNote.Length
+					_, err := my_bot.Send(video_note)
+					if err != nil {
+						ErrorCatch(err.Error(), my_bot)
+					}
+				} else {
+					msg.Text = "Bot cannot send this yet! Please, contact with creator"
+					_, err := my_bot.Send(msg)
+					if err != nil {
+						ErrorCatch(err.Error(), my_bot)
+					}
+				}
+				continue
+			} else {
+				continue
+			}
+		}
+
+		chat_id := update.Message.Chat.ID
+		msg := tgbotapi.NewMessage(chat_id, "")
+
+		switch update.Message.Command() {
+		case "start":
+			if !CheckUserReg(update.Message.From.ID, database, my_bot) {
+				UserFirstStart(update.Message.From.ID, database, my_bot)
+				msg.Text = "Hello, this is Freedom chat, where you can freely express your minds and talk with other strangers.\n\n" +
+					"To start the chat, send /go_chat command or press \"New chat\" button\n\n" +
+					"To leave the chat, send /leave_chat command or press \"Leave chat\" button\n\n" +
+					"Bot doesn't store any personal data, so chats are fully anonymous.\n\n" +
+					"If You want to check how the bot works - check my video (https://www.youtube.com/watch?v=drtAdOByW54&t=1s)\n\n" +
+					"If You have some questions or suggestions, please, feel free to contact with me, @YUART\n\n" +
+					"Also, check my Patreon page (https://www.patreon.com/artemkakun) if you want receive some bonuses from me :)\n"
+				msg.ReplyMarkup = numericKeyboard
+			} else {
+				msg.Text = "Hello, this is Freedom chat, where you can freely express your minds and talk with other strangers.\n\n" +
+					"To start the chat, send /go_chat command or press \"New chat\" button\n\n" +
+					"To leave the chat, send /leave_chat command or press \"Leave chat\" button\n\n" +
+					"Bot doesn't store any personal data, so chats are fully anonymous.\n\n" +
+					"If You want to check how the bot works - check my video (https://www.youtube.com/watch?v=drtAdOByW54&t=1s)\n\n" +
+					"If You have some questions or suggestions, please, feel free to contact with me, @YUART\n\n" +
+					"Also, check my Patreon page (https://www.patreon.com/artemkakun) if you want receive some bonuses from me :)\n"
+				msg.ReplyMarkup = numericKeyboard
+			}
+		case "go_chat":
+			if CheckUserReg(update.Message.From.ID, database, my_bot) {
+				if IsFree(update.Message.From.ID, database, my_bot) {
+					if !IsSearch(update.Message.From.ID, database, my_bot) {
+						ChangeSearch(database, update.Message.From.ID, 1, my_bot)
+						msg.Text = "Search started"
+					} else {
+						msg.Text = "You are searching already"
+					}
+				} else {
+					msg.Text = "You are chatting already"
+				}
+			} else {
+				msg.Text = "You need to /start first"
+			}
+		case "leave_chat":
+			if CheckUserReg(update.Message.From.ID, database, my_bot) {
+				if FindChat(update.Message.From.ID, database, my_bot) != 0 {
+					chat_id := FindChat(update.Message.From.ID, database, my_bot)
+					DeleteChat(update.Message.From.ID, database, my_bot)
+					ChangeState(database, update.Message.From.ID, 0, my_bot)
+					msg.Text = "You leaved a chat"
+
+					DeleteChat(chat_id, database, my_bot)
+					ChangeState(database, chat_id, 0, my_bot)
+					_, err := my_bot.Send(tgbotapi.NewMessage(int64(chat_id), "The stranger leave the chat"))
+					if err != nil {
+						ErrorCatch(err.Error(), my_bot)
+					}
+				} else {
+					msg.Text = "You are not chatting now!"
+				}
+			} else {
+				msg.Text = "You need to /start first"
+			}
+		}
+
+		_, err := my_bot.Send(msg)
+		if err != nil {
+			ErrorCatch(err.Error(), my_bot)
+		}
+	}
+}
+
+func BotInitError(err error) {
+	fmt.Println(fmt.Errorf("QueryRow failed: %w\n", err))
+	panic(err)
+}
